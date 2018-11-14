cf6616305417a3d5a6c7f8e8c34568ad528befc6
MatthewCrankshaw <mhcrankshaw2@gmail.com>
2018-11-14 14:22:30 +1300

    Final version

--- assignment5.cu
+++ assignment5.cu
@@ -13,9 +13,10 @@ using namespace std;
 
 const long MAXDIM = 10;
 const double RMIN = 2.0;
-const double RMAX = 8.0;
+const double RMAX = 7.0;
 
-
+#define MAX_THREADS 1024
+#define MAX_BLOCKS 65535
 
 
 //Global kernel code that runs on the device
@@ -31,7 +32,7 @@ __global__ void count_in(ULL *dev_count, long dev_ntotal,long dev_ndim, long dev
 
 	double rtestsq = 0;
 	long idx = 0;
-	long index[MAXDIM];
+	long index[MAXDIM+1];
 	for (long i = 0; i < dev_ndim; ++i) index[i] = 0;
 	
   	//Convert the decimal number into another base system
@@ -61,6 +62,15 @@ void err_check(cudaError_t err, char* text){
 	}
 }
 
+// Used to time code. OK for single threaded programs but not for
+// multithreaded programs. See other demos for hints at timing CUDA
+// code.
+double diffclock(clock_t clock1,clock_t clock2)
+{
+  double diffticks = clock1 - clock2;
+  return (diffticks * 1000) / CLOCKS_PER_SEC;
+}
+
 
 //calculate the power using long integers
 long powlong(long n, long k)
@@ -70,36 +80,27 @@ long powlong(long n, long k)
   for (long i = 0; i < k; ++i) p *= n;
   return p;
 }
-					
+		
+// A sequential version of the program for debugging purposes			
 void count_in_seq(ULL &count,int thread, int block,long ndim, long halfb, double rsquare, long base){ 
-
 	int num = (block * 1024) + thread;
 	double rtestsq = 0;
 	long idx = 0;
-
 	long index[MAXDIM];
-
 	for (long i = 0; i < ndim; ++i){
 		index[i] = 0;
 	}
-  	
   	while (num != 0) {
     	long rem = num % base;
     	num = num / base;
     	index[idx] = rem;
     	++idx;
   	}
-
 	for(long k = 0; k < ndim; ++k){
 		double xk = index[k] - halfb;
 		rtestsq += xk * xk;
 	}
-
-	if(rtestsq< rsquare){
-
-		//This needs to be an atomic add
-		count++;
-	}
+	if(rtestsq< rsquare) count++;
 }
 
 
@@ -107,56 +108,95 @@ void count_in_seq(ULL &count,int thread, int block,long ndim, long halfb, double
 // Main Function
 //========================================
 
-int main(int argc, char **argv){ 
-  // You can make this larger if you want
-  const long ntrials = 10;
+int main(int argc, char **argv){
 
-  for (long n = 0; n < ntrials; ++n) {
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start); 
+	cudaEventCreate(&stop);
+	float deviceTime = 0;
 
-    // Get a random value for the hypersphere radius between the two limits
-    const double radius = drand48() * (RMAX - RMIN) + RMIN;
+  	// You can make this larger if you want
+  	const long ntrials = 10;
 
-    // Get a random value for the number of dimensions between 1 and
-    // MAXDIM inclusive
-    const long  ndim = lrand48() % (MAXDIM - 1) + 1;
-    std::cout << "Trial Number " << n << " Radius " << radius << " Dimensions " << ndim << " ... " << std::endl;
+	for (long n = 0; n < ntrials; ++n) {
 
+		clock_t totalTime = clock();
 
-    //Count for counting the number of integer points that land in the circle
-    ULL count = 0;
+		// Get a random value for the hypersphere radius between the two limits
+		//const double radius = drand48() * (RMAX - RMIN) + RMIN;
+		const double radius = 1;
 
-    //Set up the variable that will be needed by the cuda kernel
-    const long halfb = static_cast<long>(floor(radius));
-  	const long base = 2 * halfb + 1;
-  	const double rsquare = radius * radius;
-  	const long ntotal = powlong(base, ndim);
+		// Get a random value for the number of dimensions between 1 and
+		// MAXDIM inclusive
+		//const long  ndim = lrand48() % (MAXDIM - 1) + 1;
+		const long ndim = ;
 
+		std::cout << "===================== Trial Number: " << n  << " =========================\n"<< 
+		"Radius: " << radius << 
+		"\nDimensions: " << ndim << std::endl;
 
-    //CUDA part
-    //=======================================================
-  	//we need to split the problem each integer point in the n dimentional space
-
-  	// Set up the device count variable which we will need to retrieve later
-  	ULL *dev_count;
-  	cudaMalloc((void**)&dev_count, sizeof(ULL));
-  	cudaMemcpy(dev_count, &count, sizeof(ULL), cudaMemcpyHostToDevice);
-
- 	//Set up the number of threads per block and blocks per grid
-  	dim3 threadsPerBlock(1024, 1, 1);
-  	dim3 blocksPerGrid(65535,7,1);
+		//Set up the variable that will be needed by the cuda kernel
+		const long halfb = static_cast<long>(floor(radius));
+			const long base = 2 * halfb + 1;
+			const double rsquare = radius * radius;
+			const long ntotal = powlong(base, ndim);
+			//Count for counting the number of integer points that land in the circle
+		ULL count = 0;
 
 
-  	cout << "Number of Threads Per Block: " << threadsPerBlock.x 	<< " " << threadsPerBlock.y << " " << threadsPerBlock.z << endl;
-  	cout << "Number of Blocks Per Grid: " 	<< blocksPerGrid.x 		<< " " << blocksPerGrid.y 	<< " " << blocksPerGrid.z 	<< endl;
-  	cout << "N Total: " 					<< ntotal 				<< endl;
+		//CUDA part
+		//=======================================================
+		//we need to split the problem each integer point in the n dimentional space
 
-  	//Run the device kernel
-  	count_in<<<blocksPerGrid, threadsPerBlock>>>(dev_count, ntotal, ndim, halfb, rsquare, base);
+		//Set up the number of threads per block and blocks per grid
 
+		int blocksx = (ntotal + MAX_THREADS - 1)/MAX_THREADS;
+		int blocksy = 1;
 
-  	//Retrieve the memory from the device for the count
-  	cudaMemcpy(&count, dev_count, sizeof(unsigned long long), cudaMemcpyDeviceToHost);
+		int rem = 1;
+		if(blocksx > MAX_BLOCKS){
+			rem = blocksx - MAX_BLOCKS;
+			blocksx = MAX_BLOCKS;
+		}
 
-    std::cout << " -> " << "count" << " " << count << "\n" << std::endl;
-  }
+		blocksy = (rem + MAX_BLOCKS - 1)/MAX_BLOCKS;
+		if(blocksy > MAX_BLOCKS){
+			blocksy = MAX_BLOCKS;
+			cout << "Too many y blocks!" << endl;
+			exit(1);
+		}
+
+		dim3 threadsPerBlock(MAX_THREADS, 1, 1);
+		dim3 blocksPerGrid(blocksx, blocksy,1);
+
+		cout << "Number of Threads Per Block: (" << threadsPerBlock.x 	<< ", " << threadsPerBlock.y << ", " << threadsPerBlock.z << ")" << endl;
+		cout << "Number of Blocks Per Grid: (" 	<< blocksPerGrid.x 		<< ", " << blocksPerGrid.y 	<< ", " << blocksPerGrid.z << ")"<< endl;
+		cout << "N Total: " 					<< ntotal 				<< endl;
+
+		cudaEventRecord(start);
+		// Set up the device count variable which we will need to retrieve later
+		ULL *dev_count;
+		cudaMalloc((void**)&dev_count, sizeof(ULL));
+		cudaMemcpy(dev_count, &count, sizeof(ULL), cudaMemcpyHostToDevice);
+
+		//Run the device kernel
+
+		count_in<<<blocksPerGrid, threadsPerBlock>>>(dev_count, ntotal, ndim, halfb, rsquare, base);
+
+		cudaEventRecord(stop);
+		cudaEventSynchronize(stop);
+		cudaEventElapsedTime(&deviceTime, start, stop);
+
+		//Retrieve the memory from the device for the count
+		cudaMemcpy(&count, dev_count, sizeof(unsigned long long), cudaMemcpyDeviceToHost);
+
+		cudaFree(dev_count);
+
+		cout << " -> " << "count" << " " << count << endl;
+		
+		cout << "Total Time: " << diffclock(clock(), totalTime) << " ms" << endl;
+		cout << "Device Elapsed Time: " << deviceTime << "ms" << endl; 
+		cout << endl;
+
+	}
 }
